b85b8377d152c50952e29341f4f90e93e556eb9b
beper223 <zashalovskiy@gmail.com>
2026-02-13 10:12:42 +0100

    admin surf

--- src/personal_forms/admin.py
+++ src/personal_forms/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.forms import ModelForm
+from django.forms.widgets import HiddenInput
 from django.forms.models import BaseInlineFormSet
+from django.db.models import Case, When, IntegerField
 
 from src.personal_forms.models import Verb, VerbForm, LearningUnit
 from src.common.choices import Tense, Pronoun
@@ -9,22 +12,34 @@ from src.common.choices import Tense, Pronoun
 TENSE_ORDER = [Tense.PRAESENS.value, Tense.PRAETERITUM.value]
 PRONOUN_ORDER = [p.value for p in Pronoun]
 
+
+class VerbFormInlineModelForm(ModelForm):
+    class Meta:
+        model = VerbForm
+        fields = ("tense", "pronoun", "form")
+        widgets = {"tense": HiddenInput()}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["pronoun"].required = False
+        self.fields["form"].required = False
+        self.fields["pronoun"].disabled = True
+
 class VerbFormInlineFormSet(BaseInlineFormSet):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if not self.is_bound:
-            # Сортируем формы по времени и местоимению перед отрисовкой
+            # Сортируем формы по местоимению перед отрисовкой
             self.forms.sort(key=self.get_form_sort_key)
 
     @staticmethod
     def get_form_sort_key(form):
         # Определяем значения для сортировки (из БД или из initial)
-        val = (form.instance.tense, form.instance.pronoun) if form.instance.pk \
-               else (form.initial.get('tense'), form.initial.get('pronoun'))
+        pronoun = form.instance.pronoun if form.instance.pk else form.initial.get("pronoun")
         try:
-            return TENSE_ORDER.index(val[0]), PRONOUN_ORDER.index(val[1])
+            return PRONOUN_ORDER.index(pronoun)
         except (ValueError, KeyError):
-            return 99, 99
+            return 99
 
     @staticmethod
     def _should_save(form):
@@ -42,21 +57,16 @@ class VerbFormInlineFormSet(BaseInlineFormSet):
 
 class BaseVerbFormInline(admin.TabularInline):
     model = VerbForm
+    form = VerbFormInlineModelForm
     formset = VerbFormInlineFormSet
-    can_delete = True
+    can_delete = False
     fields = ("pronoun", "form")
     extra = 0
-
-    def formfield_for_choice_field(self, db_field, request, **kwargs):
-        if db_field.name == "pronoun":
-            kwargs["disabled"] = True
-        return super().formfield_for_choice_field(db_field, request, **kwargs)
 
     def get_queryset(self, request):
         # Сортировка внутри каждой группы только по местоимению
         qs = super().get_queryset(request).filter(tense=self.tense_value)
         pronoun_order = [p.value for p in Pronoun]
-        from django.db.models import Case, When, IntegerField
         whens = [When(pronoun=p, then=i) for i, p in enumerate(pronoun_order)]
         return qs.annotate(sort_id=Case(*whens, output_field=IntegerField())).order_by("sort_id")
 
@@ -64,13 +74,6 @@ class BaseVerbFormInline(admin.TabularInline):
         if not obj: return 6
         existing_count = obj.forms.filter(tense=self.tense_value).count()
         return max(0, 6 - existing_count)
-
-    @staticmethod
-    def get_missing_forms(obj):
-        full_set = [{"tense": t, "pronoun": p} for t in TENSE_ORDER for p in PRONOUN_ORDER]
-        if not obj: return full_set
-        existing = set(obj.forms.values_list("tense", "pronoun"))
-        return [f for f in full_set if (f["tense"], f["pronoun"]) not in existing]
 
     def get_formset(self, request, obj=None, **kwargs):
         form_set = super().get_formset(request, obj, **kwargs)
@@ -86,16 +89,25 @@ class BaseVerbFormInline(admin.TabularInline):
 
             # Автоматически проставляем нужное время при сохранении новых форм
             def save_new(self, form, commit=True):
-                if form.cleaned_data.get("form"):
-                    instance = form.save(commit=False)
-                    instance.tense = self.instance_tense_value  # передаем из инлайна
-                    if commit: instance.save()
-                    return instance
+                if not self._should_save(form):
+                    return None
+                instance = form.save(commit=False)
+                setattr(instance, self.fk.name, self.instance)
+                instance.tense = self.instance_tense_value  # передаем из инлайна
+                if commit:
+                    instance.save()
+                return instance
 
         CustomFormSet.instance_tense_value = self.tense_value
         return CustomFormSet
 
 
+
+class PraesensVerbForm(VerbForm):
+    class Meta:
+        proxy = True  # Это не создает новую таблицу, просто дает Django "псевдоним"
+        verbose_name = "Form (Präsens)"
+        verbose_name_plural = "Konjugation: Präsens"
 
 class PraeteritumVerbForm(VerbForm):
     class Meta:
@@ -106,7 +118,7 @@ class PraeteritumVerbForm(VerbForm):
 # --- ДВА РАЗНЫХ ИНЛАЙНА ---
 
 class PraesensInline(BaseVerbFormInline):
-    model = VerbForm
+    model = PraesensVerbForm
     verbose_name = "Präsens"
     verbose_name_plural = "Konjugation: Präsens"
     tense_value = Tense.PRAESENS.value
